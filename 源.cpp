cfb3a9429278dde156c3855fe274bf11a205d872
power <2124538559@qq.com>
2025-08-31 11:29:10 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,89 @@
+#include <stdio.h>
+/*int m;
+int min(int x, int y);
+int main()
+{
+	int a, b;
+	printf("\n输入两个整数:");
+	scanf_s("%d,%d", &a, &b);
+	m = min(a, b);
+		printf("比较小的数值:%d\n", m);
+}
+int min(int x, int y)
+{
+	int t = 0;
+	if (x < y) t = x;
+	else t = y;
+	return(t);
+}
+#define pi  3.14159
+int main(void){
+	float r,c,s;
+	printf("\n输入半径r:");
+	scanf_s("%f", &r);
+	c = pi * (2 * r);
+	s = pi * (r * r);
+	printf("\n周长为:%f", c);
+	printf("\n面积为:%f", s);
+}
+int main(void)
+{
+	float a, c, s;
+	printf("\n输入边长:");
+	scanf_s("%f", &a);
+	c = a * 4;
+	s = a * a;
+	printf("\n边长:%f", c);
+	printf("\n面积:%f", s);
+}
+int main() {
+
+
+	float upper_base, lower_base, height, area;
+
+
+	// 提示用户输入梯形的上底、下底和高
+
+
+	printf("请输入梯形的上底：");
+
+
+	scanf_s("%f", &upper_base);
+
+
+	printf("请输入梯形的下底：");
+
+
+	scanf_s("%f", &lower_base);
+
+
+	printf("请输入梯形的高：");
+
+
+	scanf_s("%f", &height);
+
+
+	// 计算梯形的面积
+
+
+	area = 0.5 * (upper_base + lower_base) * height;
+
+
+	// 输出结果
+
+
+	printf("梯形的面积是：%.2f", area);
+
+
+	return 0;
+
+
+}
+int main(void) {
+	int a, b;
+	printf("输入第一件商品价格:");
+	scanf_s("%d", &a);
+	printf("\n输入第二件商品价格:");
+	scanf_s("%d", &b);
+	printf("\n总金额%d", a + b);
+}*/
